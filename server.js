07a27d21165424b437165202e5ec3d812f0988d4
Bruna <ppbruna2@gmail.com>
2016-01-24 19:25:49 -0200

    adiciona rota get para buscar todas as empresas

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 8765
 var router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var companies = [
+  { cnpj: '0000000000-191', nome_fantasia: 'Nome fantasia da empresa'  }
+]
+
+var companiesRoute = router.route('/empresas');
+
+companiesRoute.get(function(request, response) {
+  response.json(companies);
+});
 
 app.use('/api', router);
 app.listen(port, function() {
